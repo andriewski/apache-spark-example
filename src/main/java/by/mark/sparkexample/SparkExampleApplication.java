7bebd0e44e1e6bb1d5ebded4c060246f9034207c
andriewski <plambiroo@gmail.com>
2020-11-09 15:02:48 +0300

    Initializing project

---
+++ src/main/java/by/mark/sparkexample/SparkExampleApplication.java
@@ -0,0 +1,13 @@
+package by.mark.sparkexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/mark/sparkexample/SparkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package by.mark.sparkexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
